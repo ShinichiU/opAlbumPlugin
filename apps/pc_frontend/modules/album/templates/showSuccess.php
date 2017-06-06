fcbdeeb3a5a8c89fba889d8f4b36c410dbf7b3ae
Shinichi Urabe <urabe@tejimaya.com>
2017-06-06 15:49:33 +0900

    (refs #4202) Corrected so that newlines of explanatory text are defined.

--- apps/pc_frontend/modules/album/templates/showSuccess.php
+++ apps/pc_frontend/modules/album/templates/showSuccess.php
@@ -27,7 +27,7 @@
 </tr>
 <tr>
 <th><?php echo __('Description') ?></th>
-<td colspan="2"><?php echo $album->getBody() ?></td>
+<td colspan="2"><?php echo nl2br($album->getBody()) ?></td>
 </tr>
 <tr>
 <th><?php echo __('Public flag') ?></th>
