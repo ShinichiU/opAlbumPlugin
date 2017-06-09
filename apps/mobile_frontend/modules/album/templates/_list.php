e45e239e3e4f36d03bdf140fb6076289d12d2269
Shinichi Urabe <urabe@tejimaya.com>
2017-06-09 12:58:22 +0900

    (refs #4206) truncate long description in list page. (mobile_frontend)

--- apps/mobile_frontend/modules/album/templates/_list.php
+++ apps/mobile_frontend/modules/album/templates/_list.php
@@ -4,7 +4,7 @@
 <?php echo link_to($album->Member->name, '@member_profile?id='.$album->Member->id) ?>
  <?php echo link_to($album->title, 'album_show', $album) ?><br>
 <center><?php echo link_to(image_tag_sf_image($album->getCoverImage(), array('width' => '80', 'size' => '120x120')), 'album_show', $album) ?></center>
-<?php echo $album->body ?>
+<?php echo op_truncate($album->body, 30, '', 3) ?><br>
  <?php echo op_format_activity_time(strtotime($album->getCreatedAt())) ?>
 <hr color="<?php echo $op_color["core_color_11"] ?>" size="3">
 <?php endforeach; ?>
